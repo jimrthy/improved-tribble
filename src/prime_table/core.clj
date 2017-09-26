54d4ef541a6f16f302898e7e8ada7e0c06021aaf
James Gatannah <james.gatannah@gmail.com>
2017-09-26 18:48:38 -0500

    Start implementing as a lazy seq

--- src/prime_table/core.clj
+++ src/prime_table/core.clj
@@ -2,6 +2,82 @@
   (:require [clojure.edn :as edn]
             [clojure.spec.alpha :as s]))
 
+(declare prime?)
+(s/def ::prime (s/and nat-int?
+                      prime?))
+(s/def ::primes (s/coll-of prime?))
+
+(s/fdef next-prime
+        :args (s/cat :previous ::prime
+                     :existing ::primes)
+        :ret ::prime)
+(defn next-prime
+  "Returns the next prime greater than previous, using existing to test"
+  [previous existing]
+  (println "Looking for the next prime after" previous)
+  (if previous
+    (if existing
+      (reduce (fn [_ x]
+                (println (str "Does " x " satisfy? (vs "
+                              existing ")"))
+                (let [sqrt (Math/sqrt x)
+                      possible-factors (filter #(<= % sqrt) existing)]
+                  (println "Comparing against" possible-factors)
+                  ;; Filtering out even numbers is cheap.
+                  ;; TODO: Verify that it was worth it.
+                  (when (and (odd? x)
+                             (every? #(not= 0 (rem x %))
+                                     possible-factors))
+                    (reduced x))))
+              (range previous ##Inf))
+      (do
+        (assert (= 2 previous))
+        3))
+    2))
+
+(defn primes
+  "Infinite seq of prime numbers"
+  ([]
+   (primes nil nil))
+  ([previous existing]
+   (let [current (next-prime previous existing)]
+     (lazy-seq (cons (conj (list previous) existing)
+                     (primes current (if previous
+                                       (if existing
+                                         (cons previous existing)
+                                         [previous])
+                                       nil)))))))
+(comment
+  (first (primes))
+  (take 3 (primes))
+  )
+
+(s/fdef prime?
+        :args (s/cat :n nat-int?)
+        :ret boolean?)
+(defn prime?
+  "Returns true if n is prime [compared against ms]"
+  ([n]
+   ;; The fact that we have a circular dependency
+   ;; between this and primes is disturbing.
+   (let [sqrt (Math/sqrt n)
+         ms (take-while #(< sqrt %) (primes))]))
+  ([n ms]
+   (every? #(not= 0 (rem n %)) ms)))
+
+(s/fdef pick-primes
+        :args (s/cat :n nat-int?)
+        :fn (s/and (fn [{:keys [:ret]
+                         {:keys [:n]} :args}]
+                     (= n (count ret)))
+                   (fn [{:keys [:ret]}]
+                     (every? prime? ret)))
+        :ret (s/coll-of nat-int?))
+(defn pick-primes
+  "Returns a seq of the first n primes"
+  [n]
+  (throw (RuntimeException. "Write this")))
+
 (defn build-prime-table
   "Build a matrix of the products of n primes"
   [n]
--- test/prime_table/core_test.clj
+++ test/prime_table/core_test.clj
@@ -1,6 +1,43 @@
 (ns core-test
-  (:require [clojure.test :refer (are deftest is)]
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]
+            [clojure.test :refer (are deftest is)]
             [prime-table.core :as p-t]))
 
-(deftest failure
-  (is (= 0 1)))
+(deftest check-next-prime
+  (are [prev existing expected] (= (p-t/next-prime prev existing) expected)
+    nil nil 2
+    2 nil 3
+    3 [2] 5
+    5 [2 3] 7
+    7 [2 3 5] 11
+    11 [2 3 5 7] 13
+    13 [2 3 5 7 11] 17
+    17 [2 3 5 7 11 13] 19))
+
+(deftest check-prime-generation
+  (let [first-3 (take 3 (p-t/primes))]
+    (is (= [2 3 5] first-3)))
+  (let [next-4 (-> (p-t/primes)
+                   (drop 3)
+                   (take 4))]
+    (is (= [7 11 13 17] next-4))))
+
+(deftest check-prime-predicate
+  (let [primes #{2 3 5 7 11 13 17 19 23 29
+                 31 37 41 43 47 53 59 61 71}
+        test-vals (gen/sample (s/gen primes))]
+    (is (every? p-t/prime? test-vals)))
+  (let [not-primes #{ -1 0 1 4 6 8 9 10
+                     12 14 15 16 18 20 21
+                     22 24 25 26 27 28 30
+                     32 33 34 35 36 38 39
+                     40 42 44 45 46 48 49
+                     50 51 52 54 55 56 57
+                     58 61 62 63 64 65 66
+                     67 68 69 70}
+        test-vals (gen/sample (s/gen not-primes))]
+    (is (every? (complement p-t/prime? test-vals)))))
+
+(deftest check-prime-calculation
+  )
